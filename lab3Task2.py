3af1e9044ff8e290ac24f448b63959e6b75c3a61
krasilnik <90803029+krasilnik@users.noreply.github.com>
2021-11-26 16:54:51 +0200

    Update lab3Task2.py

--- lab3Task2.py
+++ lab3Task2.py
@@ -1,126 +1,167 @@
-import json
+from datetime import datetime, date
 import uuid
-from datetime import date
-import calendar
+import json
 
+ADVANCE_DATE = 60
+LATE_DATE = 10
+ADVANCE_TICKET_DISCOUNT = 0.6
+STUDENT_TICKET_DISCOUNT = 0.5
+LATE_TICKET_ADDITIONAL = 1.1
 
-class Pizza:
-    def __init__(self, name, price, ingredients):
-        self.__name = name
+class Event:
+    def __init__(self, title, date, price):
+        if not isinstance(title, str):
+            raise TypeError('Invalid type')
+        if price <= 0:
+            raise ValueError('Invalid value')
+        self.title = title
+        self.date = date
+        self.price = price
+
+    def write(self):
+        with open('events.json', 'r') as f:
+            data = json.load(f)
+
+            event = {'title': self.title,
+                     'date': self.date,
+                     'price': self.price}
+
+            data.append(event)
+
+        with open('events.json', 'w') as write_file:
+            json.dump(data, write_file, indent = 3)
+
+    def __str__(self):
+        return f'\nEvent: \n\tTitle: {self.title}\n\tDate: {self.date}\n\tPrice: {self.price}'
+
+class RegularTicket:
+    def __init__(self, price, event_title, type = "Regular ticket"):
+        if not isinstance(price, (int, float)) or not isinstance(event_title, str) or not isinstance(type, str):
+            raise TypeError('Invalid type')
+        if price <= 0:
+            raise ValueError('Invalid value')
+        self.__type = type
+        self.__number = str(uuid.uuid4().int)[:6]
         self.__price = price
-        self.__ingredients = ingredients
-
-    @property
-    def name(self):
-        return self.__name
-
-    @name.setter
-    def name(self, name):
-        if not isinstance(name, (str)):
-            raise TypeError("Name must be a string type")
-        self.__name = name
+        self.__event = event_title
 
     @property
     def price(self):
         return self.__price
 
-    @price.setter
-    def price(self, value):
-        if not isinstance(value, (int, float)):
-            raise TypeError("Pizza's price must be a digit")
-        if value <= 0:
-            raise ValueError("Pizza cant cost nothing!")
-        self.__price = value
+    @property
+    def type(self):
+        return self.__type
 
     @property
-    def ingredients(self):
-        return self.__ingredients
-
-    @ingredients.setter
-    def ingredients(self, value):
-        if not isinstance(value, (list)):
-            raise TypeError("Ingredients type must be sting")
-        self.__ingredients = value
-
-    def add_ingredient(self, ingredient):
-        self.ingredients.append(ingredient)
+    def number(self):
+        return self.__number
 
     def __str__(self):
-        return f"\t Pizza's name: {self.__name} \n\t Pizza's price: {self.__price} \n\t ingredients: {self.__ingredients} \n"
+        return f'\nTicket: \n\tType: {self.__type}\n\tNumber: {self.__number}\n\tPrice: {self.__price}\n\tEvent: ' \
+               f'{self.__event}'
 
-class MondayPizza(Pizza):
-    def __init__(self):
-        super().__init__("Neapolitan", 145, ["tomatoes", "garlic", "oregano"])
+    def write(self):
+        with open('tickets.json', 'r') as f:
+            data = json.load(f)
 
-class TuesdayPizza(Pizza):
-    def __init__(self):
-        super().__init__("Chicago", 170, ["tomato sauce", "ground beef", "pepperoni", "green peppers", "mushrooms"])
+            ticket = {'type': self.__type,
+                      'number': self.__number,
+                      'price': self.__price,
+                      'event': self.__event}
 
-class WednesdayPizza(Pizza):
-    def __init__(self):
-        super().__init__("New York-Style", 190, ["mushroom and anchovies", "Mozzarella", "pepperoni and sausage", "tomato sauce"])
+            data.append(ticket)
 
-class ThursdayPizza(Pizza):
-    def __init__(self):
-        super().__init__("Sicilian", 210, ["robust tomato sauce", "bits of tomato", "onion", "anchovies"])
+        with open('tickets.json', 'w') as write_file:
+            json.dump(data, write_file, indent = 3)
 
-class FridayPizza(Pizza):
-    def __init__(self):
-        super().__init__("Greek", 185, ["tangy tomato paste", "mozzarella and cheddar", "black olives", "red onion"])
+class AdvanceTicket(RegularTicket):
+    def __init__(self, price, event):
+        super().__init__(price * ADVANCE_TICKET_DISCOUNT, event, "Advance ticket")
 
-class SaturdayPizza(Pizza):
-    def __init__(self):
-        super().__init__("California", 240, ["chicken", "artichokes", "goat cheese", "egg"])
+class LateTicket(RegularTicket):
+    def __init__(self, price, event):
+        super().__init__(price * LATE_TICKET_ADDITIONAL, event, "Late ticket")
 
-class SundayPizza(Pizza):
-    def __init__(self):
-        super().__init__("Detroit", 160, ["pepperoni", "brick Wisconsin cheese", "tomato sause"])
-
+class StudentTicket(RegularTicket):
+    def __init__(self, price, event):
+        super().__init__(price * STUDENT_TICKET_DISCOUNT, event, "Student ticket")
 
 class Order:
     def __init__(self):
-        self.id = str(uuid.uuid4().int)
-        self.pizza = 0
+        self.selected_event = None
+        self.ticket = None
 
-    def day(self):
-        today_date = date.today()
-        return calendar.day_name[today_date.weekday()]
-
-    def your_order(self):
-        name = self.day()
-        dict = {"Monday": MondayPizza(), "Tuesday": TuesdayPizza(), "Wednesday": WednesdayPizza(),
-                "Thursday": ThursdayPizza(), "Friday": FridayPizza(), "Saturday": SaturdayPizza(),
-                "Sunday": SundayPizza()}
-        self.pizza = dict[name]
-        print(f"Today we have {self.pizza.name} pizza! \n if you wand to add an ingredient - type it. If not - type 'no'")
-        self.add()
-        self.write()
-
-    def add(self, text = ""):
-        answer = input(text)
-        if answer == 'no':
-            print("any extra ingrediets")
-            return
-        else:
-            self.pizza.add_ingredient(answer)
-        self.add("Do you want more extra ingredients? If yes - type it")
-
-    def write(self):
-        with open('pizza.json', 'r') as f:
+    def select(self):
+        with open('events.json', 'r') as f:
             data = json.load(f)
+            for i in data:
+                temp_event = Event(i['title'], i['date'], i['price'])
+                print(temp_event.__str__())
+        selection = input('\nSelect your event: ')
+        if not any(i['title'] == selection for i in data):
+            raise ValueError('Invalid input')
+        for i in data:
+            if i['title'] == selection:
+                self.selected_event = i
 
-            order = {'id': self.id,
-                     'pizza': self.pizza.name,}
+    @staticmethod
+    def get_date_difference(event_date):
+        data = datetime.now()
+        current_date = (data.year, data.month, data.day)
+        event_date = tuple(int(i) for i in reversed(event_date.split('.')))
+        if current_date > event_date:
+            raise TimeoutError('Event ended')
+        return (date(data.year, data.month, data.day) - date(event_date[0], event_date[1], event_date[2])).days
 
-            data.append(order)
+    def make_order(self):
+        ticket_type = input('\nEnter ticket type: Advanced/Student/Regular/Late\n')
+        days_difference = abs(Order.get_date_difference(self.selected_event['date']))
+        if not (ticket_type in ('Advanced', 'Student', 'Regular', 'Late')):
+            Order.make_order(self)
+        if ticket_type == 'Advanced':
+            if days_difference < ADVANCE_DATE:
+                raise TimeoutError('Sorry, you can not order advanced ticked, because '
+                                   'less than 60 days left before the event')
+            self.ticket = AdvanceTicket(self.selected_event['price'], self.selected_event['title'])
+        elif ticket_type == 'Student':
+            self.ticket = StudentTicket(self.selected_event['price'], self.selected_event['title'])
+        elif ticket_type == 'Regular':
+            self.ticket = RegularTicket(self.selected_event['price'], self.selected_event['title'])
+        elif ticket_type == 'Late':
+            if days_difference >= LATE_DATE:
+                raise TimeoutError('Sorry, you can not order late ticked, because '
+                                   'there are at least 10 days left before the event')
+            self.ticket = LateTicket(self.selected_event['price'], self.selected_event['title'])
+        self.ticket.write()
 
-        with open('pizza.json', 'w') as write_file:
-            json.dump(data, write_file, indent=3)
+    @staticmethod
+    def search_ticket(ticket_number):
+        with open('tickets.json', 'r') as f:
+            data = json.load(f)
+        if not ticket_number or not isinstance(ticket_number, str):
+            raise TypeError('Ticket ticket_number extinct')
+        if not any(i['number'] == ticket_number for i in data):
+            raise ValueError('Ticket extinct')
+        res = None
+        for i in data:
+            if i['number'] == ticket_number:
+                type = i['type']
+                price = i['price']
+                event = i['event']
+                res = f'\nTicket:\n\tType: {type}\n\tNumber: {ticket_number}\n\tPrice: {price}\n\tEvent: {event}'
+        return res
 
+    def __str__(self):
+        event_title = self.selected['title']
+        return f'\nYour order:\n\tEvent: {event_title}\n\tTicket type: {self.ticket.type}\n\t' \
+               f'Price: {self.ticket.price}\n\tNumber: {self.ticket.number}'
 
 def main():
     order = Order()
-    order.your_order()
+    order.select()
+    order.make_order()
+    print(order.search_ticket("190157"))
     print(order.__str__())
 
 main()
