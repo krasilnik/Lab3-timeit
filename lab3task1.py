a22c5646de27dba81f2cf7de41f18ed502abc2fc
krasilnik <90803029+krasilnik@users.noreply.github.com>
2021-11-26 16:56:45 +0200

    Update lab3task1.py

--- lab3task1.py
+++ lab3task1.py
@@ -1,51 +1,168 @@
 
+from datetime import datetime, date
 import uuid
+import json
+
+ADVANCE_DATE = 60
+LATE_DATE = 10
+ADVANCE_TICKET_DISCOUNT = 0.6
+STUDENT_TICKET_DISCOUNT = 0.5
+LATE_TICKET_ADDITIONAL = 1.1
 
 class Event:
-    def __init__(self, name, date):
-        self.name = name
+    def __init__(self, title, date, price):
+        if not isinstance(title, str):
+            raise TypeError('Invalid type')
+        if price <= 0:
+            raise ValueError('Invalid value')
+        self.title = title
         self.date = date
+        self.price = price
+
+    def write(self):
+        with open('events.json', 'r') as f:
+            data = json.load(f)
+
+            event = {'title': self.title,
+                     'date': self.date,
+                     'price': self.price}
+
+            data.append(event)
+
+        with open('events.json', 'w') as write_file:
+            json.dump(data, write_file, indent = 3)
 
     def __str__(self):
-        return f'{self.name}, {self.date}'
+        return f'\nEvent: \n\tTitle: {self.title}\n\tDate: {self.date}\n\tPrice: {self.price}'
 
-
-class Ticket:
-    def __init__(self, event, price, type = "Regular"):
-        self.__ticket_type = type
-        self.__id = uuid.uuid1()
-        self.__event = event
+class RegularTicket:
+    def __init__(self, price, event_title, type = "Regular ticket"):
+        if not isinstance(price, (int, float)) or not isinstance(event_title, str) or not isinstance(type, str):
+            raise TypeError('Invalid type')
+        if price <= 0:
+            raise ValueError('Invalid value')
+        self.__type = type
+        self.__number = str(uuid.uuid4().int)[:6]
         self.__price = price
+        self.__event = event_title
 
     @property
     def price(self):
         return self.__price
 
+    @property
+    def type(self):
+        return self.__type
+
+    @property
+    def number(self):
+        return self.__number
+
     def __str__(self):
-        return f"\n Ticket's type: {self.__ticket_type} \n\tTicket's id: {self.__id}\n\tEvent's name and date: {self.__event}\n\tTicket's price: {self.__price}"
+        return f'\nTicket: \n\tType: {self.__type}\n\tNumber: {self.__number}\n\tPrice: {self.__price}\n\tEvent: ' \
+               f'{self.__event}'
 
-class Student(Ticket):
-    def __init__(self, event, price):
-            super().__init__(event, price * 0.5, "Student")
+    def write(self):
+        with open('tickets.json', 'r') as f:
+            data = json.load(f)
 
-class Late(Ticket):
-    def __init__(self, event, price):
-            super().__init__(event, price * 1.2, "Late")
+            ticket = {'type': self.__type,
+                      'number': self.__number,
+                      'price': self.__price,
+                      'event': self.__event}
 
-class Advance(Ticket):
-    def __init__(self, event, price):
-            super().__init__(event, price * 0.4, "Advance")
+            data.append(ticket)
 
-def start():
-    normal_price = 150
-    some_event = Event("some_event", "15.01.2002")
-    first = Ticket(some_event, normal_price)
-    second = Student(some_event, normal_price)
-    third = Late(some_event, normal_price)
-    forth = Advance(some_event, normal_price)
-    print(first.__str__())
-    print(second.__str__())
-    print(third.__str__())
-    print(forth.__str__())
+        with open('tickets.json', 'w') as write_file:
+            json.dump(data, write_file, indent = 3)
 
-start()
+class AdvanceTicket(RegularTicket):
+    def __init__(self, price, event):
+        super().__init__(price * ADVANCE_TICKET_DISCOUNT, event, "Advance ticket")
+
+class LateTicket(RegularTicket):
+    def __init__(self, price, event):
+        super().__init__(price * LATE_TICKET_ADDITIONAL, event, "Late ticket")
+
+class StudentTicket(RegularTicket):
+    def __init__(self, price, event):
+        super().__init__(price * STUDENT_TICKET_DISCOUNT, event, "Student ticket")
+
+class Order:
+    def __init__(self):
+        self.selected_event = None
+        self.ticket = None
+
+    def select(self):
+        with open('events.json', 'r') as f:
+            data = json.load(f)
+            for i in data:
+                temp_event = Event(i['title'], i['date'], i['price'])
+                print(temp_event.__str__())
+        selection = input('\nSelect your event: ')
+        if not any(i['title'] == selection for i in data):
+            raise ValueError('Invalid input')
+        for i in data:
+            if i['title'] == selection:
+                self.selected_event = i
+
+    @staticmethod
+    def get_date_difference(event_date):
+        data = datetime.now()
+        current_date = (data.year, data.month, data.day)
+        event_date = tuple(int(i) for i in reversed(event_date.split('.')))
+        if current_date > event_date:
+            raise TimeoutError('Event ended')
+        return (date(data.year, data.month, data.day) - date(event_date[0], event_date[1], event_date[2])).days
+
+    def make_order(self):
+        ticket_type = input('\nEnter ticket type: Advanced/Student/Regular/Late\n')
+        days_difference = abs(Order.get_date_difference(self.selected_event['date']))
+        if not (ticket_type in ('Advanced', 'Student', 'Regular', 'Late')):
+            Order.make_order(self)
+        if ticket_type == 'Advanced':
+            if days_difference < ADVANCE_DATE:
+                raise TimeoutError('Sorry, you can not order advanced ticked, because '
+                                   'less than 60 days left before the event')
+            self.ticket = AdvanceTicket(self.selected_event['price'], self.selected_event['title'])
+        elif ticket_type == 'Student':
+            self.ticket = StudentTicket(self.selected_event['price'], self.selected_event['title'])
+        elif ticket_type == 'Regular':
+            self.ticket = RegularTicket(self.selected_event['price'], self.selected_event['title'])
+        elif ticket_type == 'Late':
+            if days_difference >= LATE_DATE:
+                raise TimeoutError('Sorry, you can not order late ticked, because '
+                                   'there are at least 10 days left before the event')
+            self.ticket = LateTicket(self.selected_event['price'], self.selected_event['title'])
+        self.ticket.write()
+
+    @staticmethod
+    def search_ticket(ticket_number):
+        with open('tickets.json', 'r') as f:
+            data = json.load(f)
+        if not ticket_number or not isinstance(ticket_number, str):
+            raise TypeError('Ticket ticket_number extinct')
+        if not any(i['number'] == ticket_number for i in data):
+            raise ValueError('Ticket extinct')
+        res = None
+        for i in data:
+            if i['number'] == ticket_number:
+                type = i['type']
+                price = i['price']
+                event = i['event']
+                res = f'\nTicket:\n\tType: {type}\n\tNumber: {ticket_number}\n\tPrice: {price}\n\tEvent: {event}'
+        return res
+
+    def __str__(self):
+        event_title = self.selected['title']
+        return f'\nYour order:\n\tEvent: {event_title}\n\tTicket type: {self.ticket.type}\n\t' \
+               f'Price: {self.ticket.price}\n\tNumber: {self.ticket.number}'
+
+def main():
+    order = Order()
+    order.select()
+    order.make_order()
+    print(order.search_ticket("190157"))
+    print(order.__str__())
+
+main()
